1b0d68ec67299e189d3606ecb32164d25c1a6905
Saboreq <118023809+Saboreq@users.noreply.github.com>
2024-11-07 20:26:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
